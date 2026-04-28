88841955671581c71846e3e3805c897b197ed244
DrMack97 <delgadoantolinezdavidalejandro@gmail.com>
2026-04-28 11:22:39 +0200

    dialogo

---
+++ src/Dialog.java
@@ -0,0 +1,28 @@
+public class Dialog {
+
+    public static void main(String[] args) {
+
+        //Dialogo por turnos 
+
+        //programa que simula un dialogo de un juego RPG
+        // el dialogo se guarda en un fichero que se denomina con el personaje 
+        //a la hora de mostrar el dialogo siempre habla un personaje primero contesta
+        //con el format
+
+        //el nombre que printa tiene que venir del nombre del fichero, 
+        //no se le dice al programa como se llaman
+        //debe haber al menos dos 
+        
+        /*
+            Heroe: que tal por aqui?
+            villano: pues desde que que viniste mal...
+            .
+            .
+            .
+         */
+
+        //version facil sin ENTER
+        //un poco mas compleja pulsar enter para la contestacion 
+    }
+
+}
